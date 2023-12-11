6bd34daa39e68c0207da9a808ab68da89f5a13af
Jabahahaha <misha.andreev3004@gmail.com>
2023-12-11 15:28:05 +0100

    Alien counter

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 class Alien:
+    total_aliens_created = 0
+
     def __init__(self, x_coordinate, y_coordinate):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.health = 3
+        Alien.total_aliens_created += 1
 
     def hit(self):
         self.health -= 1
