61a0234866447b35e8b16afdf6c1d06b38b7db95
Jabahahaha <misha.andreev3004@gmail.com>
2023-12-11 15:29:57 +0100

    Creating a list of aliens

--- main.py
+++ main.py
@@ -18,4 +18,14 @@ class Alien:
         self.y_coordinate = new_y_coordinate
 
     def collision_detection(self, other_object):
-        pass  # Placeholder
+        pass  # Placeholder for future implementation
+
+def new_aliens_collection(start_positions):
+    return [Alien(x, y) for x, y in start_positions]
+
+# Example usage
+alien_start_positions = [(4, 7), (-1, 0)]
+aliens = new_aliens_collection(alien_start_positions)
+
+for alien in aliens:
+    print(alien.x_coordinate, alien.y_coordinate)
